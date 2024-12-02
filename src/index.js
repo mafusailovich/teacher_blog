8d9d4e8d6f8b7a43476751c05c396bf6d12bf122
mafusailovich <vfesenko@yandex.ru>
2024-12-02 14:19:11 +0300

    добавлен слайдер swiper на главную страницу

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './index.html';
 import './main.scss';
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.achievements-slider', {
+  // configure Swiper to use modules
+  modules: [Navigation],
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
